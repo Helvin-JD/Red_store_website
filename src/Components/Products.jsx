36351f52ff2a767cd92eb1ecb92693ba837d2f77
BARATH S <barathsuthakar05@gmail.com>
2025-11-26 22:40:05 +0530

    products

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Navbar from "./Navbar";
 import "./Products.css";
 
 // products
@@ -6,16 +7,18 @@ const products = [
   //  Watches
   {
     id: 1,
-    name: "BENYAR Automatic",
+    name: "BENYAR",
     image: "https://m.media-amazon.com/images/I/814hrCDIz4L._SX679_.jpg",
-    description: "Automatic Mechanical Skeleton Watch",
+    description:
+      "BENYAR Automatic Mechanical Skeleton Leather Strap Men's Watch",
     price: "$299.99",
   },
   {
     id: 2,
-    name: "Fossil Men’s Black Steel Watch",
-    image: "https://m.media-amazon.com/images/I/61Zs5XRCVKL._SX679_.jpg",
-    description: "Minimalist analog watch with steel strap",
+    name: "Veirdo",
+    image: "https://m.media-amazon.com/images/I/616xMq9zXJL._SY741_.jpg",
+    description:
+      "Pure Cotton Men's Graphic Print Oversized Fit Half Sleeve Round Neck",
     price: "$199.99",
   },
 
@@ -30,8 +33,9 @@ const products = [
   {
     id: 4,
     name: "Adidas Running Shoes",
-    image: "https://m.media-amazon.com/images/I/61iTev1qg6L._SY695_.jpg",
-    description: "Lightweight breathable mesh running shoes",
+    image: "https://m.media-amazon.com/images/I/71NPlnbjxeL._SX695_.jpg",
+    description:
+      "Men Agora 1.0 Multisport Lightweight breathable mesh running shoes",
     price: "$129.99",
   },
 
@@ -46,15 +50,15 @@ const products = [
   {
     id: 6,
     name: "Puma Slim Fit T-Shirt",
-    image: "https://m.media-amazon.com/images/I/51jPzRp0T9L._SX679_.jpg",
-    description: "Black slim-fit T-shirt with printed graphic",
+    image: "https://m.media-amazon.com/images/I/61VbsN6cjJL._SX679_.jpg",
+    description: "Blue slim-fit T-shirt with printed graphic",
     price: "$19.99",
   },
   {
     id: 7,
-    name: "HRX Active Wear Tee",
-    image: "https://m.media-amazon.com/images/I/71cQ8v5n2TL._SX679_.jpg",
-    description: "Training T-shirt for men with quick-dry technology",
+    name: "Boldfit Gym Bag",
+    image: "https://m.media-amazon.com/images/I/71+aDeSfS5L._SX679_.jpg",
+    description:"Men & Women Organized Duffle Bag with Adjustable Strap",
     price: "$15.99",
   },
 
@@ -69,8 +73,9 @@ const products = [
   {
     id: 9,
     name: "Dennis Lingo Shirt",
-    image: "https://m.media-amazon.com/images/I/71O84WkEJVL._SY879_.jpg",
-    description: "Premium cotton slim-fit stylish checkered shirt",
+    image: "https://m.media-amazon.com/images/I/71NCBXev2YL._SY879_.jpg",
+    description:
+      "Premium cotton slim-fit Premium Men's Casual Shirt Regular Fit",
     price: "$24.99",
   },
 
@@ -78,15 +83,17 @@ const products = [
   {
     id: 10,
     name: "Jockey Track Pants",
-    image: "https://m.media-amazon.com/images/I/61g7NxvYZ9L._SY741_.jpg",
+    image: "https://m.media-amazon.com/images/I/71vUWVMOSBL._SX466_.jpg",
     description: "Soft cotton stretchable joggers for men",
     price: "$22.99",
   },
   {
     id: 11,
-    name: "Adidas Slim Fit Joggers",
-    image: "https://m.media-amazon.com/images/I/61mvMRGsiDL._SY741_.jpg",
-    description: "Comfortable lightweight polyester joggers",
+    name: "Hexagon Laptop Bag",
+    image:
+      "https://m.media-amazon.com/images/I/313wG+n2AbL._SY300_SX300_QL70_FMwebp_.jpg",
+    description:
+      "Laptop Bag With 15.6” Laptop Sleeve, Shoe Compartment & USB Charging ",
     price: "$39.99",
   },
 
@@ -94,7 +101,7 @@ const products = [
   {
     id: 12,
     name: "HRX Men’s Ankle Socks",
-    image: "https://m.media-amazon.com/images/I/71NLdXePpnL._SX679_.jpg",
+    image: "https://m.media-amazon.com/images/I/812QkIIFUaL._SX679_.jpg",
     description: "Pack of 3 cotton rich black/white/grey socks",
     price: "$6.99",
   },
@@ -102,38 +109,40 @@ const products = [
   // Smart Watches
   {
     id: 13,
-    name: "Fire-Boltt Smartwatch",
-    image: "https://m.media-amazon.com/images/I/71lL2Z9GZ2L._SX679_.jpg",
-    description: "Bluetooth calling smartwatch with HD display",
+    name: "Atowak Automatic",
+    image: "https://m.media-amazon.com/images/I/71COV3ihJAL._SX679_.jpg",
+    description:"Men Cobra Steel Silver Automatic Watch & Carbon Fiber Patches Dial",
     price: "$49.99",
   },
   {
     id: 14,
-    name: "Noise ColorFit Pro 4",
-    image: "https://m.media-amazon.com/images/I/61c6+FDRWKL._SX679_.jpg",
-    description: "1.8 inch AMOLED smartwatch with health tracking",
-    price: "$59.99",
+    name: "Stuhrling Original",
+    image: "https://m.media-amazon.com/images/I/818L0-9YYLL._SX679_.jpg",
+    description: "Skeleton Automatic Watch for Men & Mechanical Watch Movement",
+    price: "$1999.99",
   },
 
   // Fashion
   {
     id: 15,
-    name: "Men’s Casual Sneakers",
-    image: "https://m.media-amazon.com/images/I/71MEH4Y0HLL._SX679_.jpg",
-    description: "High-top comfortable daily wear sneakers",
+    name: "The Souled Store Store",
+    image: "https://m.media-amazon.com/images/I/51cdyJYlVDL._SY695_.jpg",
+    description:
+      "The Souled Store Naruto Shippuden: Sasuke Men Purple High Top Sneakers",
     price: "$34.99",
   },
   {
     id: 16,
-    name: "Men’s Sport Shoes",
-    image: "https://m.media-amazon.com/images/I/61efnq4ZPEL._SX679_.jpg",
-    description: "Breathable lightweight running shoes",
+    name: "U.S. POLO ASSN",
+    image: "https://m.media-amazon.com/images/I/71TCP-Tq2VL._SY695_.jpg",
+    description: "USPA|Stefan| Stylish Casual Sneaker Shoes for Man",
     price: "$44.99",
   },
 ];
 export default function Products() {
   return (
     <>
+      <Navbar/>
       <div className="container">
         <h2>Products</h2>
         {products.map((product) => (
